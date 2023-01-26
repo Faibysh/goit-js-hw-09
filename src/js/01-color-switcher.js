2e705a89c842c285ee771ad21f198eebfeeb3469
Faibysh <faybysh.b@gmail.com>
2023-01-26 20:01:57 +0100

    finished task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const body = document.querySelector("body");
+
+let interval = 0;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+
+function onStartBtnClick(){
+  interval = setInterval(getRandomHexColor, 1000);
+  startBtn.setAttribute("disabled", true);
+}
+
+function onStopBtnClick() {
+  startBtn.removeAttribute("disabled");
+  clearInterval(interval);
+}
+  
+
+function getRandomHexColor() {
+   body.style.backgroundColor=`#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
